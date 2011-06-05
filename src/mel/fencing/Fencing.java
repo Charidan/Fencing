009499beddc6937cf9e34c5e540f3b2de5939ae3
Mel Nicholson <mhnicholson@yahoo.com>
2011-06-05 15:50:59 -0700

    Initial project creation

---
+++ src/mel/fencing/Fencing.java
@@ -0,0 +1,13 @@
+package mel.fencing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Fencing extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
