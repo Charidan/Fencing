50128f63df0128ac2193207942b5e7ea03dc4b39
Richard Nicholson <Charidan@HITPOINT>
2011-06-05 16:18:05 -0700

    Added Card and Deck; implemented a shuffle method.

---
+++ src/mel/fencing/Card.java
@@ -0,0 +1,10 @@
+package mel.fencing;
+
+public class Card
+{
+    private final int value;
+    
+    public Card(int value) { this.value = value; }
+
+    public int getValue() { return value; }
+}
---
+++ src/mel/fencing/Deck.java
@@ -0,0 +1,35 @@
+package mel.fencing;
+
+import java.util.ArrayList;
+
+public class Deck
+{
+    private ArrayList<Card> cards = new ArrayList<Card>();
+    
+    public Deck()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            cards.add(new Card(1));
+            cards.add(new Card(2));
+            cards.add(new Card(3));
+            cards.add(new Card(4));
+            cards.add(new Card(5));
+        }
+        shuffle();
+    }
+
+    public void shuffle()
+    {
+        int size = cards.size();
+        for(int i = 0; i < size; i++)
+        {
+            int temp = (int) (Math.random()*(size-i))+i;
+            Card c = cards.get(temp);
+            cards.set(temp, cards.get(i));
+            cards.set(i, c);
+        }
+    }
+    
+    public Card getCard(int index) { return cards.get(index); }
+}
