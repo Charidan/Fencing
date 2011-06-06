b7e7b95efe5b96d15c1df636f8484dd87cbeb4ff
Mel Nicholson <mhnicholson@yahoo.com>
2011-06-05 17:16:49 -0700

    Adding the Strip View

---
+++ src/mel/fencing/StripView.java
@@ -0,0 +1,22 @@
+package mel.fencing;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class StripView extends View
+{
+    public StripView(Context context)
+    {
+        super(context);
+        init();
+    }
+    
+    public StripView(Context context, AttributeSet attr)
+    {
+        super(context, attr);
+        init();
+    }
+    
+    void init() {}
+}
