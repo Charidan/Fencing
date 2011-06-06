742f03307a3c8f671a80453833626e4adfbf174c
Charidan <Charidan@Linuxeratops>
2011-06-06 10:26:16 -0700

    Made the New Game button shuffle the deck and display. Made the Help button spew text.

--- src/mel/fencing/Fencing.java
+++ src/mel/fencing/Fencing.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Fencing extends Activity
 {
     Deck deck = new Deck();
+    TextView header;
+    TextView footer;
     
     /** Called when the activity is first created. */
     @Override
@@ -16,8 +19,9 @@ public class Fencing extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        TextView tv = (TextView) findViewById( R.id.header );
-        tv.setText(deck.toString());
+        header = (TextView) findViewById( R.id.header );
+        header.setText(deck.toString());
+        footer = (TextView) findViewById( R.id.footer );
     }
     
     @Override
@@ -26,5 +30,32 @@ public class Fencing extends Activity
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.layout.menu, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.newGame:
+            newGame();
+            return true;
+        case R.id.help:
+            showHelp();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    public void newGame()
+    {
+	footer.setText("Hoy, look, a New Game!");
+	deck.shuffle();
+	header.setText(deck.toString());
+    }
+    
+    public void showHelp()
+    {
+	footer.setText("If you have to ask, you don't already know.");
     }
 }
